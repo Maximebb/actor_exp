ee320b609ad6bf154c6f55f36f9cdab729879b21
Maxime Beaudry <mbeaudry@GENETEC.COM>
2018-07-10 22:18:56 -0400

    Change from stateless actor to message proc

--- Actor.Common/Interfaces.fs
+++ Actor.Common/Interfaces.fs
@@ -10,10 +10,11 @@ type IStatefulActor<'message> =
     abstract member Stop        : unit -> Task
     abstract member Dispose     : unit -> unit
 
-type IStatelessActor<'identity, 'message> =
+type IMessageProc<'identity, 'message, 'state> =
+    /// This method should throw if the implementation deems the operation a failure.
+    /// Ex. If it implements optimistic concurrency, this should throw if
+    /// failure conditions are met. It can be number of dirty write, 
     abstract member SendAsync   : 'identity -> 'message -> CancellationToken -> Task
-    abstract member Send        : 'identity -> 'message -> bool
-    abstract member Ping        : unit -> Task<bool>
 
 type IStateProvider<'identity,'state>  =
     abstract member GetStateAsync   : CancellationToken -> 'identity -> Task<'state>
---
+++ StatelessActor.Infrastructure/ConnectedStateProvider.fs
@@ -0,0 +1,13 @@
+﻿namespace StatelessActor.Infrastructure
+
+open Actor.Common
+open System.Threading
+open System.Threading.Tasks
+
+module ConnectedStateProvider =
+    let boilerplate = 1
+
+    //type ProviderImpl<'identity, 'state when 'identity : comparison> () =
+    //    interface IStateProvider<'identity,'state> with 
+    //        member x.SaveStateAsync cancelToken id s = //Read from cosmos db
+    //        member x.GetStateAsync cancelToken id = //Save to cosmos db
