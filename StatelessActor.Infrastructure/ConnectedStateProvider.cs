522a0267692f870052a404410932c7109776076e
Maxime Beaudry <mbeaudry@GENETEC.COM>
2018-09-15 11:06:24 -0400

    Retarget .net standard 2.0 and move some files around

---
+++ StatelessActor.Infrastructure/ConnectedStateProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Azure.Documents.Client;
+
+namespace StatelessActor.Infrastructure
+{
+    public class ConnectedStateProvider
+    {
+        private const string EndPoint = "";
+        private const string PrimaryKey = "";
+        private DocumentClient m_client;
+
+        public ConnectedStateProvider()
+        {
+            m_client = new DocumentClient( new Uri( EndPoint ), PrimaryKey );
+        }
+
+        
+    }
+}
